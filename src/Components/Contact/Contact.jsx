09a32bd4a04f0e316d97ba2c9b87c35978d95cfa
kartikay varun <varunkartikay0325@gmail.com>
2024-10-16 23:24:23 +0530

    added contact page

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useRef } from 'react';
 import "./Contact.css"
 import Professional from '../Projects/Professional'
 import Social from '../Projects/Social'
+import { MdOutlineMailOutline } from "react-icons/md";
+import { FaWhatsapp } from "react-icons/fa";
+import emailjs from "emailjs-com"
 
 
 const Contact = () => {
+
+const form = useRef();
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  emailjs
+    .sendForm('service_boyyo1l', 'template_w7rf9rt', form.current, 'CUB040N3sdQVjW2Ik')
+    .then(
+      () => {
+        console.log('SUCCESS!');
+      },
+      (error) => {
+        console.log('FAILED...', error.text);
+      },
+    );
+};
+
+
+
   return (
    <section id="contact" className='section_contact'>
    <h1>Kartikay varun</h1>
@@ -21,6 +44,32 @@ const Contact = () => {
     <Social/>
   </div>
 </div>
+
+<div className='contact_form'>
+
+
+  <div className='contact_box'>
+<article className='contact_option'>
+<MdOutlineMailOutline className="contact_icons" />
+<h4>Email</h4>
+<a href="mailto:varunkartikay0325@gmail.com" target='_blank'>Send a message</a>
+</article>
+
+<article className='contact_option'>
+<FaWhatsapp  className="contact_icons"/>
+<h4>WhatsApp</h4>
+<a href="https://api.whatsapp.com/send?phone=+918887894226" target='_blank'>Send a message</a>
+</article>
+  </div>
+
+  <form ref={form}  onSubmit={sendEmail} className='lets_talk'>
+    <input type="text" name='name' placeholder='Your Full Name' required />
+    <input type="email" name='email' placeholder='Email' required />
+    <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+    <button type='submit' className=' btn btn-primary talk-btn'>Send Message</button>
+  </form>
+</div>
+
 
         </div>
    </section>
