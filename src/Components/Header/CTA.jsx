01c2d72ffb28dd861d8cfa6263f42d9aa371c7f5
kartikay varun <varunkartikay0325@gmail.com>
2024-10-19 17:56:40 +0530

    added resumne to download

--- src/Components/Header/CTA.jsx
+++ src/Components/Header/CTA.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { TfiDownload } from "react-icons/tfi";
+import kkResume from "../../assets/Resume.pdf"
 
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href="www.google.com" download className="btn">   {/* -IMPORT CV FROM ASSETS FOLDER - */}
+      <a href={kkResume} download className="btn">   {/* -IMPORT CV FROM ASSETS FOLDER - */}
         <strong>Resume 
         <span>
           <TfiDownload className="dwnld" />
