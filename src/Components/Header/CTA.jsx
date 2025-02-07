666c9dddf09d58d00d6bfd38d3aa4024a2633fb1
KartikayV13 <133855971+KartikayV13@users.noreply.github.com>
2025-02-07 12:13:09 +0530

    Update CTA.jsx

--- src/Components/Header/CTA.jsx
+++ src/Components/Header/CTA.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { TfiDownload } from "react-icons/tfi";
-import kkResume from "../../assets/Resume.pdf"
+import kkResume from "../../assets/KKResume..pdf"
 
 
 const CTA = () => {
